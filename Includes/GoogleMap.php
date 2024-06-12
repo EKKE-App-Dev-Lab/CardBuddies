7169c3ab745984bf1be2309c2d921b913f7c4dbc
Zokni1Coder <erikkovacs0000@gmail.com>
2024-06-13 01:11:47 +0200

    E1-001 Google Map

---
+++ Includes/GoogleMap.php
@@ -0,0 +1,12 @@
+<?php include "./AdditionalPHP/checkAccess.php"; ?>
+
+<div class="map">
+    <div class="subtitle">
+        <h2>ITT TALÁLHATSZ MEG MINKET</h2>
+    </div>
+
+    <div class="google-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4445.274923533052!2d20.384436052609672!3d47.90157274773578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47408d84f6c8bf51%3A0x80a9f6a0d2a20313!2sEszterh%C3%A1zy%20K%C3%A1roly%20Katolikus%20Egyetem%20C%20%C3%89p%C3%BClet!5e1!3m2!1shu!2shu!4v1705270824280!5m2!1shu!2shu" width="100%" height="450" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+        </iframe>
+    </div>
+</div>
