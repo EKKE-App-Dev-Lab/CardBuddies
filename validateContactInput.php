1aea875feebcf14a3c830277b429ddaee00b78af
Zokni1Coder <erikkovacs0000@gmail.com>
2024-06-14 01:43:16 +0200

    E1-001 kontakt validáló algoritmus

---
+++ validateContactInput.php
@@ -0,0 +1,79 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $tel = $order = "";
+
+    $recaptchaCriteria = "";
+    $sendCriteria = "";
+    $errorCriteria = "";
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $name = $_POST['customerName'];
+        $email = $_POST['customerEmail'];
+        $message = $_POST['customerMessage'];
+
+        if(isset($_POST['customerPhone']))
+        {
+            $phone = $_POST['customerPhone'];
+            $tel = "<br>Phone: ". test_input($phone) . "<br>";
+        }
+
+        if(isset($_POST['orderNumber']))
+        {
+            $orderNumber = $_POST['orderNumber'];
+            $order = "<br>Order Number: " . test_input($orderNumber) . "<br>";
+        }
+
+        $to = "CardBuddies.hungary@gmail.com";
+        $subject = "Üzenet - küldte $name";
+        $message = "<b>Üzenet: $message</b><br>Telefonszám: <b>$tel</b><br><b>Rendelés: $order</b>";
+        $altMessage = $message . $tel . $order;
+        $headers = "From: $email \r\n";
+        $headers .= "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+//        mail($to, $subject, $note, $headers);
+        $mail = new PHPMailer(true);
+
+        try {
+            $mail->CharSet = 'utf-8';
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'CardBuddies.hungary@gmail.com';
+            $mail->Password = 'wrlbddenzoendmbz';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465;
+
+            $mail->setFrom('CardBuddies.hungary@gmail.com', 'CardBuddies');
+            $mail->addAddress($to);
+
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $message;
+            $mail->AltBody = $altMessage;
+
+            $mail->send();
+            $mail->smtpClose();
+
+            echo "Üzenet elküldve";
+        } catch (Exception $e) {
+            echo "Az üzenet küldése sikertelen. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+?>
