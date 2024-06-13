a7285158c1b2ae096c0ce4e315630fa0c06aa0b2
Zokni1Coder <erikkovacs0000@gmail.com>
2024-06-14 01:17:55 +0200

    E1-003 Rólunk oldal szerkezete

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php 
+    define('Access', TRUE);
+    include "./AdditionalPHP/startSession.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en-MU">
+    <head>
+        <meta charset="utf-8">
+        <title>CardBuddies | RÓLUNK</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--CSS File-->   
+        <link rel="stylesheet" type="text/css" href="Common.css">
+        <link rel="stylesheet" type="text/css" href="Atish.css">
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/0e16635bd7.js" crossorigin="anonymous"></script>
+        <!--BOXICONOK-->
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+        <!-- Animált CSS -->
+        <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    </head>
+
+    <body>
+        <?php $page = 'about';?>
+
+        
+    </body>
+</html>
