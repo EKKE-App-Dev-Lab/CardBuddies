7fa4ffdf93475493059856312af5f83999debd77
Zokni1Coder <erikkovacs0000@gmail.com>
2024-06-14 01:29:55 +0200

    E1-003 Csapatunk szekció egy része

--- about.php
+++ about.php
@@ -133,6 +133,48 @@
         </div>
     </div>
     <!--End Kitüntetések-->
+    <!--Start Csapatinfó-->
+    <div class="team-section">
+        <div class="team-title">
+            <span>Aranykezű Mestereink</span>
+        </div>
+
+        <div class="subtitle">
+            <h2>CSAPATUNK</h2>
+        </div>
+
+            
+        <div class="all-helper-info">
+            <div class="helper-individual">
+                <div class="helper-group helper1">
+                    <div class="helper-pic-group">
+                        <div class="helper-pic"></div>
+                    </div>
+
+                    <div class = "helper-more-about">
+                        <p class="name"><b>ALAPOS JÓZSEF</b></p>
+                        <p class="hierarchy">TÁRSALAPÍTÓ</p>
+                        <p class="description">Az éjjel soha nem érhet véget, varázsolj nekünk valami szépet...</p>
+                    </div>
+        
+                    <div class="helper-social-media">
+                        <div class="social-media">
+                            <span class="facebook">
+                                    <a href=#><i class="fab fa-facebook-square"></i></a>
+                            </span>
+                            <span class="twitter">
+                                <a href=#><i class="fab fa-twitter-square"></i></a>
+                            </span>
+                            <span class="instagram">
+                                <a href=#><i class="fab fa-instagram-square"></i></a>
+                            </span>
+                            <span class="pinterest">
+                                <a href=#><i class="fab fa-pinterest-square"></i></a>
+                            </span>
+                        </div>
+                    </div> 
+                </div>
+            </div>                          
 
 
     </body>
