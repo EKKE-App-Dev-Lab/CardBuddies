3452748e7b5b2b7e8dff42d4edae263d4dccc946
schon <schon@DESKTOP-0H8OTQT>
2024-06-15 17:25:42 +0200

    E1-006 sql query kategóriák lekérdezése

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $pass = "";
-$dbname = "vinylmasterdb";
+$dbname = "cardbuddiesdb";
 $conn = mysqli_connect($host, $username, $pass, $dbname);
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
