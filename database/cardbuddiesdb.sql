8adc2879b9891b29fb0cc2e689e7be0530c1bb27
joooki82 <61735967+joooki82@users.noreply.github.com>
2024-06-15 09:23:15 +0200

    E1-002_Adatbázis  - nationality tábla létrehozása

--- database/cardbuddiesdb.sql
+++ database/cardbuddiesdb.sql
@@ -2,3 +2,26 @@ drop database if exists `cardbuddiesdb`;
 create database if not exists `cardbuddiesdb`;
 use `cardbuddiesdb`;
 
+CREATE TABLE `products`
+(
+    `productID`  bigint(20)   NOT NULL AUTO_INCREMENT,
+    `jatekosnev` varchar(255) not null,
+    `nemzetiseg` varchar(255) not null,
+    `csapat`     varchar(255) not null,
+    `tamadopont` int          not null,
+    `vedopont`   int          not null,
+    `leiras`     text         not null,
+    `allapot`    varchar(255) not null,
+    `ar`         int          not null,
+    `kartyakep`  varchar(255) null,
+    PRIMARY KEY (`productID`)
+);
+
+CREATE TABLE `nationality`
+(
+    `nationalityID` bigint(20)  NOT NULL,
+    `p_nat_name` varchar(30) NOT NULL,
+    `p_nat_desc` text        NOT NULL
+);
+
+
